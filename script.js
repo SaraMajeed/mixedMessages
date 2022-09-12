da96cfe8c4f3aa1cd317e4a4328556d45b9ebb21
Sara Majeed <saramajeed@Saras-MacBook-Pro.local>
2022-09-12 16:02:50 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const message = {
+    animal: ['goat', 'cat', 'dog', 'cow', 'sheep', 'bat', 'horse', 'fox', 'squirrel', 'buffalo', 'mouse', 'pigeon', 'lion', 'monkey', 'elephant'],
+    habitat: ['wetlands', 'tundra', 'savanna', 'tropical rainforest', 'desert', 'arctic', 'coral reef', 'grasslands'],
+    sounds: ['screech', 'growl', 'buzz', 'chirp', 'meow', 'bark', 'moo', 'quack', 'squeak', 'neigh', 'roar', 'sing']
+}
+
+
