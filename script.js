da6e5a31ffafc55d87d7a3de43a70e262c782df5
Sara Majeed <smajeed434@gmail.com>
2022-09-12 16:21:35 +0100

    created random number function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const message = {
     animal: ['goat', 'cat', 'dog', 'cow', 'sheep', 'bat', 'horse', 'fox', 'squirrel', 'buffalo', 'mouse', 'pigeon', 'lion', 'monkey', 'elephant'],
     habitat: ['wetlands', 'tundra', 'savanna', 'tropical rainforest', 'desert', 'arctic', 'coral reef', 'grasslands'],
-    sounds: ['screech', 'growl', 'buzz', 'chirp', 'meow', 'bark', 'moo', 'quack', 'squeak', 'neigh', 'roar', 'sing']
+    sound: ['screech', 'growl', 'buzz', 'chirp', 'meow', 'bark', 'moo', 'quack', 'squeak', 'neigh', 'roar', 'sing']
 }
 
-
+const randNumber = num => Math.floor(Math.random() * num)
